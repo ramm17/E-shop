1519df0e3527644cd85a97ab076909c239ea5cfd
Ramm <ramgopal.rana.161@gmail.com>
2022-12-13 09:21:26 +0545

    initial commit

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from .views import *
+from django.urls import path
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('category/<slug>', CategoryView.as_view(), name='category'),
+    path('product_details/<slug>', ProductDetailView.as_view(), name='product_details'),
+    path('product_review', product_review, name='product_review'),
+
+]
